aec09da56ecba58e04ec6ad089d8c59c977cd2a0
Pedro Fellipe <49622290+PedroLimaDev@users.noreply.github.com>
2022-10-22 15:00:25 -0300

    Add assets and lint
    Add asset files and ESLint formatting configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 export function App() {
-  return (
-    <div className="App">
-     <h1>Hello World</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello World</h1>
+		</div>
+	);
 }
