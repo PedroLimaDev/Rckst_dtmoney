a0011ce25fb99beeb0e1ca10a3355ae84838e467
Pedro Fellipe <49622290+PedroLimaDev@users.noreply.github.com>
2022-10-22 15:55:20 -0300

    Add Header and Dashboad
    Add Header and Dashboard components

---
+++ src/components/dashboard/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { StyledDashboard } from './dashboard-styles';
+
+const Dashboard = () => {
+	return (
+		<StyledDashboard>
+      teste
+		</StyledDashboard>
+	);
+};
+
+export default Dashboard;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     --red: #e52e4d;
     --blue: #5429cc;
     --blue-light: #6933ff;
+    --white: #fff;
     
     --text-title: #363f5f;
     --text-body: #969cb3;
@@ -33,6 +34,15 @@ export const GlobalStyle = createGlobalStyle`
     background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  } 
 
   button {
     cursor: pointer;
